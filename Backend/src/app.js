f9999569c00c6b343942a06fdb7cd70095452b15
AnShAwAsThY10 <anshawasthy1234@gmail.com>
2026-05-10 23:15:04 +0530

    CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,10 +5,8 @@ const authRoutes = require('./routes/auth.routes');
 const musicRoutes = require('./routes/music.routes');
 const app = express();
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://lizzn.vercel.app', 'https://lizzn-qggx.onrender.com'], // Add common frontend origins, or use true to dynamically allow
-    credentials: true,
-}));
+app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
