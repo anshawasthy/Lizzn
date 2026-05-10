d619e64698e997a7fee5fcf935c604e80fe8d714
AnShAwAsThY10 <anshawasthy1234@gmail.com>
2026-05-10 23:19:11 +0530

    CORS1

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,11 @@ const authRoutes = require('./routes/auth.routes');
 const musicRoutes = require('./routes/music.routes');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://lizzn-rg71.vercel.app/",
+    credentials: true
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
