872d60bde7f03f782ca2420f090a950fced8cebb
AnShAwAsThY10 <anshawasthy1234@gmail.com>
2026-05-11 17:11:59 +0530

    Add dotenv support and configure BASE_URL in api.js; update package.json and package-lock.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from 'react';
+require('dotenv').config();
 import { AuthProvider, AuthContext } from './contexts/AuthContext';
 import { PlayerProvider } from './contexts/PlayerContext';
 import Home from "./pages/Home";
--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 
-  baseURL: 'https://lizzn-ggqx.onrender.com/',
+  baseURL: process.env.BASE_URL,
 
   withCredentials: true,
 });
